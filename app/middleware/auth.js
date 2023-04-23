7ebdd66904e709d07ad1d709c6f1da22aa808c93
Nguyễn Hữu Trọng <75382199+huutrong1101@users.noreply.github.com>
2023-04-24 00:42:02 +0700

    update code

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,34 +1,32 @@
-const asyncHandler  = require("./async");
-var jwt 			      = require('jsonwebtoken');
-const systemConfig 	= require(__path_configs + 'system');
-var ErrorResponse   = require(__path_utils + 'ErrorResponse')
-var UserModel       = require(__path_models + 'user')
+const asyncHandler = require("./async");
+var jwt = require("jsonwebtoken");
+const systemConfig = require(__path_configs + "system");
+var ErrorResponse = require(__path_utils + "ErrorResponse");
+var UserModel = require(__path_models + "user");
 
-exports.protect = asyncHandler (async (req,res,next) => {
-    let token = '';
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-      token = req.headers.authorization.split(' ')[1]
-    }else if(req.cookies.token){
-      token = req.cookies.token
-    }
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token = req.cookies.token;
 
-    if(!token) return next(new ErrorResponse(401,'Vui lòng đăng nhập tài khoản'));
+  if (!token)
+    return next(new ErrorResponse(401, "Vui lòng đăng nhập tài khoản 1 "));
 
-    try {
-        // decode token
-        const decoded = jwt.verify(token,systemConfig.JWT_SECRET);
-        req.user = await UserModel.listItems({id : decoded.id} , {task : 'one'});
-        next();
-    } catch (err) {
-        return next(new ErrorResponse(401,'Vui lòng đăng nhập tài khoản'));
-    }
-})
+  try {
+    // decode token
+    const decoded = jwt.verify(token, systemConfig.JWT_SECRET);
+    console.log(decoded);
+    req.user = await UserModel.listItems({ id: decoded.id }, { task: "one" });
+    console.log(req.user);
+    next();
+  } catch (err) {
+    return next(new ErrorResponse(401, "Vui lòng đăng nhập tài khoản 2"));
+  }
+});
 
 exports.authorize = (...roles) => {
-    return (req,res,next) => {
-      if(!roles.includes(req.user.role)){
-        return next(new ErrorResponse(403,'Bạn không có quyền truy cập'));
-      }
-      next();
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new ErrorResponse(403, "Bạn không có quyền truy cập"));
     }
-}
+    next();
+  };
+};
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,100 +1,117 @@
-var express         = require('express');
-var router          = express.Router();
-const systemConfig  = require(__path_configs + 'system');
-var asyncHandler    = require(__path_middleware + 'async')
-var ErrorResponse   = require(__path_utils + 'ErrorResponse')
-var {protect}       = require(__path_middleware + 'auth')
+var express = require("express");
+var router = express.Router();
+const systemConfig = require(__path_configs + "system");
+var asyncHandler = require(__path_middleware + "async");
+var ErrorResponse = require(__path_utils + "ErrorResponse");
+var { protect } = require(__path_middleware + "auth");
 
+const controllerName = "auth";
+const MainModel = require(__path_models + controllerName);
+const MainValidate = require(__path_validates + "password");
 
-const controllerName = 'auth';
-const MainModel 	= require(__path_models + controllerName);
-const MainValidate	= require(__path_validates + 'password');
-
-
-router.post('/register',asyncHandler(async (req,res, next) => {
-    let err = await validateReq(req,res, next);
-    if(!err){
-        const token = await MainModel.create(req.body);
-        if(token){
-            saveCookieResponse(res,201,token);
-        }
+router.post(
+  "/register",
+  asyncHandler(async (req, res, next) => {
+    let err = await validateReq(req, res, next);
+    if (!err) {
+      const token = await MainModel.create(req.body);
+      if (token) {
+        saveCookieResponse(res, 201, token);
+      }
     }
-}))
+  })
+);
 
-router.post('/login',asyncHandler(async (req,res, next) => {
-    const token = await MainModel.login(req.body,res);
-    if(token){
-        saveCookieResponse(res,201,token);
+router.post(
+  "/login",
+  asyncHandler(async (req, res, next) => {
+    const token = await MainModel.login(req.body, res);
+    if (token) {
+      saveCookieResponse(res, 201, token);
     }
-}))
+  })
+);
 
-router.get('/me',protect,asyncHandler(async (req,res, next) => {
+router.get(
+  "/me",
+  protect,
+  asyncHandler(async (req, res, next) => {
     res.status(200).json({
-        success : true,
-        user : req.user
-    })
-}))
+      success: true,
+      user: req.user,
+    });
+  })
+);
 
-router.post('/forgotPassword',asyncHandler(async (req,res, next) => {
-    const result = await MainModel.forgotPassword(req.body)
-    if(!result) res.status(401).json({success : true , massage : "Email không tồn tại"})
+router.post(
+  "/forgotPassword",
+  asyncHandler(async (req, res, next) => {
+    const result = await MainModel.forgotPassword(req.body);
+    if (!result)
+      res.status(401).json({ success: true, massage: "Email không tồn tại" });
     res.status(200).json({
-        success : true,
-        data : result
-    })
-}))
+      success: true,
+      data: result,
+    });
+  })
+);
 
-router.post('/resetPassword/:resetToken',asyncHandler(async (req,res, next) => {
-    let err = await validateReq(req,res, next);
-    if(!err){
-        const user = await MainModel.resetPassword({resetToken : req.params.resetToken , password : req.body.password})
-        if(!user) res.status(401).json({success : true , massage : "Không tồn tại Token"})
-        res.status(201)
-        .json({
-            success : true,
-            user
-        })
+router.post(
+  "/resetPassword/:resetToken",
+  asyncHandler(async (req, res, next) => {
+    let err = await validateReq(req, res, next);
+    if (!err) {
+      const user = await MainModel.resetPassword({
+        resetToken: req.params.resetToken,
+        password: req.body.password,
+      });
+      if (!user)
+        res.status(401).json({ success: true, massage: "Không tồn tại Token" });
+      res.status(201).json({
+        success: true,
+        user,
+      });
     }
-}))
+  })
+);
 
-router.get('/logout',protect,asyncHandler(async (req,res, next) => {
-    res.status(200)
-    .cookie('token','none',{
-        expirers : new Date (
-            Date.now() + 10 * 1000
-        ),
-        httpOnly : true
-    })
-    .json({
-        success : true,
-    })
-}))
-
+router.get(
+  "/logout",
+  protect,
+  asyncHandler(async (req, res, next) => {
+    res
+      .status(200)
+      .cookie("token", "none", {
+        expirers: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+      })
+      .json({
+        success: true,
+      });
+  })
+);
 
 module.exports = router;
 
-const validateReq = async (req,res,next) => {
-    let err = await MainValidate.validator(req);
-    if(Object.keys(err).length > 0){
-        next(new ErrorResponse(400,err));
-        return true;
-    }
-    return false;
-}
+const validateReq = async (req, res, next) => {
+  let err = await MainValidate.validator(req);
+  if (Object.keys(err).length > 0) {
+    next(new ErrorResponse(400, err));
+    return true;
+  }
+  return false;
+};
 
-const saveCookieResponse = (res,statusCode,token) => {
-    const options = {
-        expirers : new Date (
-            Date.now() + systemConfig.COOKIE_EXP * 24 * 60 * 60 * 1000
-        ),
-        httpOnly : true
-    }
+const saveCookieResponse = (res, statusCode, token) => {
+  const options = {
+    expirers: new Date(
+      Date.now() + systemConfig.COOKIE_EXP * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
 
-    res.status(statusCode)
-    .cookie('token',token,options)
-    .json({
-        success : true,
-        token
-    })
-}
-
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
+};
