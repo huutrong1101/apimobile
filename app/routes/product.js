7b6d3620e466fb8b9a7f9b8d0cb9d051e875c6ee
Nguyễn Hữu Trọng <75382199+huutrong1101@users.noreply.github.com>
2023-05-12 01:17:23 +0700

    update code

--- app/routes/product.js
+++ app/routes/product.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var router = express.Router();
 var asyncHandler = require(__path_middleware + "async");
-var ErrorResponse = require(__path_utils + "ErrorResponse");
-var { protect, authorize } = require(__path_middleware + "auth");
 var uploadCloud = require("../middleware/uploader");
 const cloudinary = require("cloudinary").v2;
 
@@ -39,37 +37,27 @@ router.get(
 
 router.post(
   "/add",
-  protect,
   uploadCloud.single("image"),
   asyncHandler(async (req, res, next) => {
     const fileData = req.file;
 
-    let err = await validateReq(req, res, next);
-    if (err) {
-      if (fileData) {
-        cloudinary.uploader.destroy(fileData.filename);
-      }
-    } else if (!err) {
-      const data = await MainModel.create({
-        ...req.body,
-        image: fileData.path,
-      });
-      res.status(201).json({
-        success: true,
-        data: data,
-      });
-    }
+    const data = await MainModel.create({
+      ...req.body,
+      image: fileData?.path,
+    });
+
+    res.status(201).json({
+      success: true,
+      data: data,
+    });
   })
 );
 
 router.put(
   "/edit/:id",
-  protect,
   uploadCloud.single("image"),
   asyncHandler(async (req, res, next) => {
     const fileData = req.file;
-
-    let err = await validateReq(req, res, next);
 
     if (err) {
       if (fileData) {
@@ -96,7 +84,6 @@ router.put(
 
 router.delete(
   "/delete/:id",
-  protect,
   asyncHandler(async (req, res, next) => {
     const data = await MainModel.deleteItem(
       { id: req.params.id },
@@ -128,12 +115,3 @@ router.put(
 );
 
 module.exports = router;
-
-const validateReq = async (req, res, next) => {
-  let err = await MainValidate.validator(req);
-  if (Object.keys(err).length > 0) {
-    next(new ErrorResponse(400, err));
-    return true;
-  }
-  return false;
-};
